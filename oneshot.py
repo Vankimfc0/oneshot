43acbeb2cae22452162992bf86aa20098c93df32
fulvius31 <mail@alessandrosangiorgi.net>
2023-12-02 12:55:55 -0600

    Add static pins from db (csv) Label in green all the networks which bssid is found on the csv Remove some of unused code
    Ref #1

--- oneshot.py
+++ oneshot.py
@@ -16,6 +16,7 @@ import statistics
 import csv
 from pathlib import Path
 from typing import Dict
+import csv
 
 
 class NetworkAddress:
@@ -92,7 +93,7 @@ class WPSpin:
     def __init__(self):
         self.ALGO_MAC = 0
         self.ALGO_EMPTY = 1
-        self.ALGO_STATIC = 2
+        self.ALGO_STATIC_DB = 2
 
         self.algos = {'pin24': {'name': '24-bit PIN', 'mode': self.ALGO_MAC, 'gen': self.pin24},
                       'pin28': {'name': '28-bit PIN', 'mode': self.ALGO_MAC, 'gen': self.pin28},
@@ -102,29 +103,7 @@ class WPSpin:
                       'pinASUS': {'name': 'ASUS PIN', 'mode': self.ALGO_MAC, 'gen': self.pinASUS},
                       'pinAirocon': {'name': 'Airocon Realtek', 'mode': self.ALGO_MAC, 'gen': self.pinAirocon},
                       # Static pin algos
-                      'pinEmpty': {'name': 'Empty PIN', 'mode': self.ALGO_EMPTY, 'gen': lambda mac: ''},
-                      'pinCisco': {'name': 'Cisco', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 1234567},
-                      'pinBrcm1': {'name': 'Broadcom 1', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 2017252},
-                      'pinBrcm2': {'name': 'Broadcom 2', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 4626484},
-                      'pinBrcm3': {'name': 'Broadcom 3', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 7622990},
-                      'pinBrcm4': {'name': 'Broadcom 4', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 6232714},
-                      'pinBrcm5': {'name': 'Broadcom 5', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 1086411},
-                      'pinBrcm6': {'name': 'Broadcom 6', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 3195719},
-                      'pinAirc1': {'name': 'Airocon 1', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 3043203},
-                      'pinAirc2': {'name': 'Airocon 2', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 7141225},
-                      'pinDSL2740R': {'name': 'DSL-2740R', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 6817554},
-                      'pinRealtek1': {'name': 'Realtek 1', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 9566146},
-                      'pinRealtek2': {'name': 'Realtek 2', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 9571911},
-                      'pinRealtek3': {'name': 'Realtek 3', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 4856371},
-                      'pinUpvel': {'name': 'Upvel', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 2085483},
-                      'pinUR814AC': {'name': 'UR-814AC', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 4397768},
-                      'pinUR825AC': {'name': 'UR-825AC', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 529417},
-                      'pinOnlime': {'name': 'Onlime', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 9995604},
-                      'pinEdimax': {'name': 'Edimax', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 3561153},
-                      'pinThomson': {'name': 'Thomson', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 6795814},
-                      'pinHG532x': {'name': 'HG532x', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 3425928},
-                      'pinH108L': {'name': 'H108L', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 9422988},
-                      'pinONO': {'name': 'CBN ONO', 'mode': self.ALGO_STATIC, 'gen': lambda mac: 9575521}}
+                      'pinGeneric': {'name': 'Static', 'mode': self.ALGO_STATIC_DB, 'static': []}}
 
     @staticmethod
     def checksum(pin):
@@ -157,35 +136,6 @@ class WPSpin:
         pin = str(pin) + str(self.checksum(pin))
         return pin.zfill(8)
 
-    def getAll(self, mac, get_static=True):
-        """
-        Get all WPS pin's for single MAC
-        """
-        res = []
-        for ID, algo in self.algos.items():
-            if algo['mode'] == self.ALGO_STATIC and not get_static:
-                continue
-            item = {}
-            item['id'] = ID
-            if algo['mode'] == self.ALGO_STATIC:
-                item['name'] = 'Static PIN — ' + algo['name']
-            else:
-                item['name'] = algo['name']
-            item['pin'] = self.generate(ID, mac)
-            res.append(item)
-        return res
-
-    def getList(self, mac, get_static=True):
-        """
-        Get all WPS pin's for single MAC as list
-        """
-        res = []
-        for ID, algo in self.algos.items():
-            if algo['mode'] == self.ALGO_STATIC and not get_static:
-                continue
-            res.append(self.generate(ID, mac))
-        return res
-
     def getSuggested(self, mac):
         """
         Get all suggested WPS pin's for single MAC
@@ -196,12 +146,16 @@ class WPSpin:
             algo = self.algos[ID]
             item = {}
             item['id'] = ID
-            if algo['mode'] == self.ALGO_STATIC:
-                item['name'] = 'Static PIN — ' + algo['name']
+            if algo['mode'] == self.ALGO_STATIC_DB:
+                for pins_static in self.algos['pinGeneric']['static']:
+                    if pins_static.isdigit():
+                        new_item = {'name': 'Static PIN DB', 'pin': pins_static}
+                        res.append(new_item)
             else:
                 item['name'] = algo['name']
-            item['pin'] = self.generate(ID, mac)
-            res.append(item)
+                item['pin'] = self.generate(ID, mac)
+                res.append(item)
+        self.algos['pinGeneric']['static'].clear()
         return res
 
     def getSuggestedList(self, mac):
@@ -220,49 +174,27 @@ class WPSpin:
             return res[0]
         else:
             return None
-
+        
+    def append_from_pin_csv(self, pin_file_path, mac):
+        with open(pin_file_path, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                if  mac.startswith(row[1]):
+                    self.algos['pinGeneric']['static'].append(row[0])
+                
     def _suggest(self, mac):
         """
         Get algos suggestions for single MAC
+        All the algos will be returned since they can work sometimes
+        The static pins will be added only if they are included in the csv for that specific mac
         Returns the algo ID
         """
-        mac = mac.replace(':', '').upper()
-        algorithms = {
-            'pin24': ('04BF6D', '0E5D4E', '107BEF', '14A9E3', '28285D', '2A285D', '32B2DC', '381766', '404A03', '4E5D4E', '5067F0', '5CF4AB', '6A285D', '8E5D4E', 'AA285D', 'B0B2DC', 'C86C87', 'CC5D4E', 'CE5D4E', 'EA285D', 'E243F6', 'EC43F6', 'EE43F6', 'F2B2DC', 'FCF528', 'FEF528', '4C9EFF', '0014D1', 'D8EB97', '1C7EE5', '84C9B2', 'FC7516', '14D64D', '9094E4', 'BCF685', 'C4A81D', '00664B', '087A4C', '14B968', '2008ED', '346BD3', '4CEDDE', '786A89', '88E3AB', 'D46E5C', 'E8CD2D', 'EC233D', 'ECCB30', 'F49FF3', '20CF30', '90E6BA', 'E0CB4E', 'D4BF7F4', 'F8C091', '001CDF', '002275', '08863B', '00B00C', '081075', 'C83A35', '0022F7', '001F1F', '00265B', '68B6CF', '788DF7', 'BC1401', '202BC1', '308730', '5C4CA9', '62233D', '623CE4', '623DFF', '6253D4', '62559C', '626BD3', '627D5E', '6296BF', '62A8E4', '62B686', '62C06F', '62C61F', '62C714', '62CBA8', '62CDBE', '62E87B', '6416F0', '6A1D67', '6A233D', '6A3DFF', '6A53D4', '6A559C', '6A6BD3', '6A96BF', '6A7D5E', '6AA8E4', '6AC06F', '6AC61F', '6AC714', '6ACBA8', '6ACDBE', '6AD15E', '6AD167', '721D67', '72233D', '723CE4', '723DFF', '7253D4', '72559C', '726BD3', '727D5E', '7296BF', '72A8E4', '72C06F', '72C61F', '72C714', '72CBA8', '72CDBE', '72D15E', '72E87B', '0026CE', '9897D1', 'E04136', 'B246FC', 'E24136', '00E020', '5CA39D', 'D86CE9', 'DC7144', '801F02', 'E47CF9', '000CF6', '00A026', 'A0F3C1', '647002', 'B0487A', 'F81A67', 'F8D111', '34BA9A', 'B4944E'),
-            'pin28': ('200BC7', '4846FB', 'D46AA8', 'F84ABF'),
-            'pin32': ('000726', 'D8FEE3', 'FC8B97', '1062EB', '1C5F2B', '48EE0C', '802689', '908D78', 'E8CC18', '2CAB25', '10BF48', '14DAE9', '3085A9', '50465D', '5404A6', 'C86000', 'F46D04', '3085A9', '801F02'),
-            'pinDLink': ('14D64D', '1C7EE5', '28107B', '84C9B2', 'A0AB1B', 'B8A386', 'C0A0BB', 'CCB255', 'FC7516', '0014D1', 'D8EB97'),
-            'pinDLink1': ('0018E7', '00195B', '001CF0', '001E58', '002191', '0022B0', '002401', '00265A', '14D64D', '1C7EE5', '340804', '5CD998', '84C9B2', 'B8A386', 'C8BE19', 'C8D3A3', 'CCB255', '0014D1'),
-            'pinASUS': ('049226', '04D9F5', '08606E', '0862669', '107B44', '10BF48', '10C37B', '14DDA9', '1C872C', '1CB72C', '2C56DC', '2CFDA1', '305A3A', '382C4A', '38D547', '40167E', '50465D', '54A050', '6045CB', '60A44C', '704D7B', '74D02B', '7824AF', '88D7F6', '9C5C8E', 'AC220B', 'AC9E17', 'B06EBF', 'BCEE7B', 'C860007', 'D017C2', 'D850E6', 'E03F49', 'F0795978', 'F832E4', '00072624', '0008A1D3', '00177C', '001EA6', '00304FB', '00E04C0', '048D38', '081077', '081078', '081079', '083E5D', '10FEED3C', '181E78', '1C4419', '2420C7', '247F20', '2CAB25', '3085A98C', '3C1E04', '40F201', '44E9DD', '48EE0C', '5464D9', '54B80A', '587BE906', '60D1AA21', '64517E', '64D954', '6C198F', '6C7220', '6CFDB9', '78D99FD', '7C2664', '803F5DF6', '84A423', '88A6C6', '8C10D4', '8C882B00', '904D4A', '907282', '90F65290', '94FBB2', 'A01B29', 'A0F3C1E', 'A8F7E00', 'ACA213', 'B85510', 'B8EE0E', 'BC3400', 'BC9680', 'C891F9', 'D00ED90', 'D084B0', 'D8FEE3', 'E4BEED', 'E894F6F6', 'EC1A5971', 'EC4C4D', 'F42853', 'F43E61', 'F46BEF', 'F8AB05', 'FC8B97', '7062B8', '78542E', 'C0A0BB8C', 'C412F5', 'C4A81D', 'E8CC18', 'EC2280', 'F8E903F4'),
-            'pinAirocon': ('0007262F', '000B2B4A', '000EF4E7', '001333B', '00177C', '001AEF', '00E04BB3', '02101801', '0810734', '08107710', '1013EE0', '2CAB25C7', '788C54', '803F5DF6', '94FBB2', 'BC9680', 'F43E61', 'FC8B97'),
-            'pinEmpty': ('E46F13', 'EC2280', '58D56E', '1062EB', '10BEF5', '1C5F2B', '802689', 'A0AB1B', '74DADA', '9CD643', '68A0F6', '0C96BF', '20F3A3', 'ACE215', 'C8D15E', '000E8F', 'D42122', '3C9872', '788102', '7894B4', 'D460E3', 'E06066', '004A77', '2C957F', '64136C', '74A78E', '88D274', '702E22', '74B57E', '789682', '7C3953', '8C68C8', 'D476EA', '344DEA', '38D82F', '54BE53', '709F2D', '94A7B7', '981333', 'CAA366', 'D0608C'),
-            'pinCisco': ('001A2B', '00248C', '002618', '344DEB', '7071BC', 'E06995', 'E0CB4E', '7054F5'),
-            'pinBrcm1': ('ACF1DF', 'BCF685', 'C8D3A3', '988B5D', '001AA9', '14144B', 'EC6264'),
-            'pinBrcm2': ('14D64D', '1C7EE5', '28107B', '84C9B2', 'B8A386', 'BCF685', 'C8BE19'),
-            'pinBrcm3': ('14D64D', '1C7EE5', '28107B', 'B8A386', 'BCF685', 'C8BE19', '7C034C'),
-            'pinBrcm4': ('14D64D', '1C7EE5', '28107B', '84C9B2', 'B8A386', 'BCF685', 'C8BE19', 'C8D3A3', 'CCB255', 'FC7516', '204E7F', '4C17EB', '18622C', '7C03D8', 'D86CE9'),
-            'pinBrcm5': ('14D64D', '1C7EE5', '28107B', '84C9B2', 'B8A386', 'BCF685', 'C8BE19', 'C8D3A3', 'CCB255', 'FC7516', '204E7F', '4C17EB', '18622C', '7C03D8', 'D86CE9'),
-            'pinBrcm6': ('14D64D', '1C7EE5', '28107B', '84C9B2', 'B8A386', 'BCF685', 'C8BE19', 'C8D3A3', 'CCB255', 'FC7516', '204E7F', '4C17EB', '18622C', '7C03D8', 'D86CE9'),
-            'pinAirc1': ('181E78', '40F201', '44E9DD', 'D084B0'),
-            'pinAirc2': ('84A423', '8C10D4', '88A6C6'),
-            'pinDSL2740R': ('00265A', '1CBDB9', '340804', '5CD998', '84C9B2', 'FC7516'),
-            'pinRealtek1': ('0014D1', '000C42', '000EE8'),
-            'pinRealtek2': ('007263', 'E4BEED'),
-            'pinRealtek3': ('08C6B3',),
-            'pinUpvel': ('784476', 'D4BF7F0', 'F8C091'),
-            'pinUR814AC': ('D4BF7F60',),
-            'pinUR825AC': ('D4BF7F5',),
-            'pinOnlime': ('D4BF7F', 'F8C091', '144D67', '784476', '0014D1'),
-            'pinEdimax': ('801F02', '00E04C'),
-            'pinThomson': ('002624', '4432C8', '88F7C7', 'CC03FA'),
-            'pinHG532x': ('00664B', '086361', '087A4C', '0C96BF', '14B968', '2008ED', '2469A5', '346BD3', '786A89', '88E3AB', '9CC172', 'ACE215', 'D07AB5', 'CCA223', 'E8CD2D', 'F80113', 'F83DFF'),
-            'pinH108L': ('4C09B4', '4CAC0A', '84742A4', '9CD24B', 'B075D5', 'C864C7', 'DC028E', 'FCC897'),
-            'pinONO': ('5C353B', 'DC537C')
-        }
+        self.append_from_pin_csv('./pins.csv', mac.upper())
+        
         res = []
-        for algo_id, masks in algorithms.items():
-            if mac.startswith(masks):
-                res.append(algo_id)
+        for algo_id in self.algos:
+            res.append(algo_id)
+
         return res
 
     def pin24(self, mac):
@@ -861,6 +793,13 @@ class WiFiScanner:
         except FileNotFoundError:
             self.stored = []
 
+    def checkvuln_from_pin_csv(self, pin_file_path, mac):
+        with open(pin_file_path, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                if  mac.startswith(row[1]):
+                    return True
+                
     def iw_scanner(self) -> Dict[int, dict]:
         """Parsing iw scan results"""
         def handle_network(line, result, networks):
@@ -1018,7 +957,7 @@ class WiFiScanner:
                 print(colored(line, color='yellow'))
             elif network['WPS locked']:
                 print(colored(line, color='red'))
-            elif self.vuln_list and (model in self.vuln_list):
+            elif (self.vuln_list and (model in self.vuln_list)) or self.checkvuln_from_pin_csv("./pins.csv", network['BSSID']):
                 print(colored(line, color='green'))
             else:
                 print(line)
@@ -1063,7 +1002,7 @@ def die(msg):
 
 def usage():
     return """
-OneShotPin 0.0.2 (c) 2017 rofl0r, modded by drygdryg
+OneShotPin 0.0.2 (c) 2017 rofl0r and drygdryg, modded by fulvius31
 
 %(prog)s <arguments>
 
@@ -1099,7 +1038,7 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(
-        description='OneShotPin 0.0.2 (c) 2017 rofl0r, modded by drygdryg',
+        description='OneShotPin 0.0.2 (c) 2017 rofl0r and drygdryg, modded by fulvius31',
         epilog='Example: %(prog)s -i wlan0 -b 00:90:4C:C1:AC:21 -K'
         )
 
